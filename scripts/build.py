56792e09a0f3af2bca60b6624857ad0a08e6f9d3
Marr11317 <remimarche@gmail.com>
2020-07-01 00:38:51 -0400

    initial commit

---
+++ scripts/build.py
@@ -0,0 +1,25 @@
+import fontforge
+
+def generateWithFeatures(font, fea, name, dest="./redist/"):
+    # make sure no old lookups remain
+    for l in font.gpos_lookups:
+        font.removeLookup(l)
+    for l in font.gsub_lookups:
+        font.removeLookup(l)
+    font.mergeFeature(fea)
+    font.familyname = name
+    font.fontname = name
+    font.appendSFNTName("English (US)", "Family", name)
+    font.appendSFNTName("English (US)", "Fullname", name)
+    font.generate(dest + name + ".otf")
+
+
+if __name__ == "__main__":
+    f = fontforge.open("./src/Figurato.sfd")
+
+    print("generating Figurato.otf")
+    generateWithFeatures(f, "./src/figurato_T.fea", "Figurato")
+    print("generating FiguratoB.otf")
+    generateWithFeatures(f, "./src/figurato_B.fea", "FiguratoB")
+
+    f.close()
